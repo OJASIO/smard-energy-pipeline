cb2ac037197aff50f601dba6b7defe71d54428ea
OJASIO <100004636@srh-heidelberg.org>
2026-06-14 22:56:58 +0000

    Fix dbt command order in stream monitor DAG

--- airflow/dags/smard_stream_monitor.py
+++ airflow/dags/smard_stream_monitor.py
@@ -7,7 +7,7 @@ VENV = "/home/usr_100004636_srh_heidelberg_org/airflow_venv/bin/python3"
 DBT  = "/home/usr_100004636_srh_heidelberg_org/airflow_venv/bin/dbt"
 JAVA = "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 && export PATH=$JAVA_HOME/bin:$PATH"
 ENV  = JAVA + " && export SPARK_LOCAL_IP=127.0.0.1 && export GOOGLE_CLOUD_PROJECT=data-management-2-498012 && export PIPELINE_ENV=prod"
-DBT_CMD = DBT + " --project-dir " + BASE + "/dbt/smard_pipeline --profiles-dir ~/.dbt --target prod"
+DBT_FLAGS = "--project-dir " + BASE + "/dbt/smard_pipeline --profiles-dir ~/.dbt --target prod"
 
 default_args = {
     "owner": "smard_pipeline",
@@ -48,7 +48,7 @@ with DAG(
 
     dbt_refresh = BashOperator(
         task_id="dbt_refresh_silver_gold",
-        bash_command=ENV + " && " + DBT_CMD + " run --select silver gold",
+        bash_command=ENV + " && " + DBT + " run " + DBT_FLAGS + " --select silver gold",
     )
 
     poll_energy >> poll_weather >> spark_bronze >> spark_silver >> dbt_refresh
